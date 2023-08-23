11992331e071bbeea49545c5f18985a4060f84d5
springtimeofyouth <md.toufik@iitg.ac.in>
2023-08-23 20:26:04 +0530

    Added Project Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="MdToufik",
+    author_email="md.toufik@iitg.ac.in",
+    packages=find_packages(),
+    install_requires=[],
+)
